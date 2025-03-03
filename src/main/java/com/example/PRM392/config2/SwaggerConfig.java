9734601bb6f4a72bf206b83d873934a89ed5c3fe
hantn127 <hanntn072003@gmail.com>
2025-03-03 19:41:26 +0700

    setup server database and deploy

---
+++ src/main/java/com/example/PRM392/config2/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.example.PRM392.config2;
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis((Predicate<RequestHandler>) RequestHandlerSelectors.basePackage("com.example.PRM392.controller"))
+                .paths((Predicate<String>) PathSelectors.any())
+                .build();
+    }
+}
